9232b6df6124287fc7f3062048d5b291ea695aac
Maryana Alhawamdah <maryana.alhawamdah8@gmail.com>
2023-08-15 02:23:10 +0300

    re

--- app.js
+++ app.js
@@ -39,60 +39,50 @@ if(flag==0){
         console.log(data);
         
         data.map(element => {
-            //................ div view ...............
+
             let userDiv = document.createElement('div');
             let idP = document.createElement('p');
             let nameP = document.createElement('p');
             let emailP = document.createElement('p');
             let passwordP = document.createElement('p');
             let birthDateP = document.createElement('p');
-            let createDateP = document.createElement('p');
-            let lastLoginP = document.createElement('p');
 
+            
             idP.textContent ="ID : " + element['id'];
-            nameP.textContent ="Name : " +  element['firstName'] + " " + element['middleName'] + " " + element['lastName'] + " " + element['familyName'];
+            nameP.textContent ="Name : " +  element['fName'] + " " + element['mName'] + " " + element['lName'] + " " + element['familyName'];
             emailP.textContent="Email : " + element['email'];
-            passwordP.textContent="Password : " + element['password'];
+            passwordP.textContent="Password : " + element['Password'];
             birthDateP.textContent="BirthDate : " + element['bDate'];
-            createDateP.textContent="Create Date : " + element['createDate'];
-            lastLoginP.textContent="Last Login : " + element['lastLogin'];
 
+            
             userDiv.appendChild(idP);
             userDiv.appendChild(nameP);
             userDiv.appendChild(emailP);
             userDiv.appendChild(passwordP);
             userDiv.appendChild(birthDateP);
-            userDiv.appendChild(createDateP);
-            userDiv.appendChild(lastLoginP);
             superDiv.appendChild(userDiv);
             userDiv.style.border='1px solid black';
             userDiv.style.padding='1%';
-            //.................table view...............
+
             let userRow = document.createElement('tr');
             let idD = document.createElement('td');
             let nameD = document.createElement('td');
             let emailD = document.createElement('td');
             let passwordD = document.createElement('td');
             let birthDateD = document.createElement('td');
-            let createDateD = document.createElement('td');
-            let lastLoginD = document.createElement('td');
 
             idD.textContent =element['id'];
-            nameD.textContent =element['firstName'] + " " + element['middleName'] + " " + element['lastName'] + " " + element['familyName'];
+            nameD.textContent =element['fName'] + " " + element['mName'] + " " + element['lName'] + " " + element['familyName'];
             emailD.textContent=element['email'];
-            passwordD.textContent=element['password'];
+            passwordD.textContent=element['Password'];
             birthDateD.textContent=element['bDate'];
-            createDateD.textContent=element['createDate'];
-            lastLoginD.textContent=element['lastLogin'];
 
             userRow.appendChild(idD);
             userRow.appendChild(nameD);
             userRow.appendChild(emailD);
             userRow.appendChild(passwordD);
             userRow.appendChild(birthDateD);
-            userRow.appendChild(createDateD);
-            userRow.appendChild(lastLoginD);
-            table.appendChild(userRow);
+            tableBody.appendChild(userRow);
         })
         })
     .catch(error=>{
